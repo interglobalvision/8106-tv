3b3617415119b83e5851cbb753498f3f29271675
cas <casska@gmail.com>
2016-02-10 18:52:45 -0600

    add basic twitter feed markup

--- partials/twitter.php
+++ partials/twitter.php
@@ -1,11 +1,27 @@
 <?php 
   $twitter_handle = IGV_get_option( '_igv_twitter_handle' ); 
 ?>
-<section class="row">
+<section id="twitter-feed" class="">
 
   <?php
     if ($twitter_handle) {
-      echo '@' . $twitter_handle;
+      $twitter_feed = get_twitter_feed($twitter_handle);
+      //pr($twitter_feed);
+
+      $url_regex = "@(https?://([-\w\.]+[-\w])+(:\d+)?(/([\w/_\.#-]*(\?\S+)?[^\.\s])?)?)@";
+      foreach($twitter_feed as $index => $twitter_item) {
+
+        // Clean twit text from urls
+        $twit_text = preg_replace($url_regex, '', $twitter_item->text);
+
+      ?>
+
+      <div class="">
+        <a href="#"><?php echo $twit_text; ?></a>
+      </div>
+
+      <?php
+      }
     }
   ?>
 
