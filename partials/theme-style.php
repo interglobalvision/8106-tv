c3c50300eca4351ee49bb4dc9df83e937e102c30
cas <casska@gmail.com>
2016-02-16 17:59:23 -0600

    add theme color field and styles

---
+++ partials/theme-style.php
@@ -0,0 +1,29 @@
+<?php
+// Get theme color
+$theme_color = IGV_get_option( '_igv_theme_color');
+?>
+<style>
+.theme-bg {
+  background-color: <?php echo $theme_color; ?>;
+}
+
+.theme-grad-bg {
+  background: #ffffff;
+  background: -moz-linear-gradient(top, #ffffff 0%, <?php echo $theme_color; ?> 100%);
+  background: -webkit-linear-gradient(top, #ffffff 0%,<?php echo $theme_color; ?> 100%);
+  background: linear-gradient(to bottom, #ffffff 0%,<?php echo $theme_color; ?> 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffffff', endColorstr='<?php echo $theme_color; ?>',GradientType=0 );
+}
+
+.theme-hard-grad-bg {
+  background: #ffffff;
+  background: -moz-linear-gradient(top,  #ffffff 0%, #edf4f2 12%, <?php echo $theme_color; ?> 100%);
+  background: -webkit-linear-gradient(top,  #ffffff 0%,#edf4f2 12%,<?php echo $theme_color; ?> 100%);
+  background: linear-gradient(to bottom,  #ffffff 0%,#edf4f2 12%,<?php echo $theme_color; ?> 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffffff', endColorstr='<?php echo $theme_color; ?>',GradientType=0 );
+}
+
+.theme-border-color {
+  border-color: <?php echo $theme_color; ?>;
+}
+</style>
