66e70fcc8befe4da53705896d11657fbea7e8be8
Patrick Best <patrickbest@patrickbest.com>
2016-02-24 11:00:09 -0600

    add ids to search fields

--- searchform.php
+++ searchform.php
@@ -3,10 +3,10 @@
     <div class="col s2">
     </div>
     <div class="col s16">
-      <input type="search" class="search-field" placeholder="keyword" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+      <input type="search" id="search-field" placeholder="keyword" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
     </div>
     <div class="col s4">
-      <button type="submit" class="search-submit"><span class="genericon genericon-search"></span></button>
+      <button type="submit" id="search-submit"><span class="genericon genericon-search"></span></button>
     </div>
   </div>
 </form>
