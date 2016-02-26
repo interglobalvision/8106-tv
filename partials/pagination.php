125df493263ed8ec00942bcfd141cbb1fb85943c
Patrick Best <patrickbest@patrickbest.com>
2016-02-26 13:58:00 -0600

    add styled pagination

--- partials/pagination.php
+++ partials/pagination.php
@@ -1,20 +1,38 @@
 <?php
-if( get_next_posts_link() || get_previous_posts_link() ) {
+if (get_next_posts_link() || get_previous_posts_link()) {
+  $previous = get_previous_posts_link('← Más Nuevo');
+  $next = get_next_posts_link('Más Viejo →');
 ?>
   <!-- post pagination -->
-  <nav id="pagination">
+  <nav id="pagination" class="row u-align-center font-italic">
 <?php
-$previous = get_previous_posts_link('Newer');
-$next = get_next_posts_link('Older');
-if ($previous) {
-  echo $previous;
-}
-if ($previous && $next) {
-  echo ' &mdash; ';
-}
-if ($next) {
-  echo $next;
-}
+  if ($previous) {
+?>
+    <div class="col s8 theme-border-color pagination-block">
+      <h3><?php echo $previous; ?></h3>
+    </div>
+<?php
+  } else {
+?>
+    <div class="col s8"></div>
+<?php
+  }
+?>
+    <div class="col s8 theme-border-color pagination-block">
+      <h3><?php echo paginate_links(array('prev_next' => false,)); ?></h3>
+    </div>
+<?php
+  if ($next) {
+?>
+    <div class="col s8 theme-border-color pagination-block">
+     <h3> <?php echo $next; ?></h3>
+    </div>
+<?php
+  } else {
+?>
+    <div class="col s8"></div>
+<?php
+  }
 ?>
   </nav>
 <?php
