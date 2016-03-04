6a65e5a646f90771ca50dd6950607b72925ef6cb
Patrick Best <patrickbest@patrickbest.com>
2016-03-04 13:58:40 -0600

    show thumb not ad on portadazza single header

--- functions.php
+++ functions.php
@@ -31,8 +31,9 @@ if( function_exists( 'add_image_size' ) ) {
   add_image_size( 'admin-thumb', 150, 150, false );
   add_image_size( 'opengraph', 1200, 630, true );
 
-  add_image_size( 'name', 199, 299, true );
   add_image_size( 'small-thumb', 124, 75, true );
+
+  add_image_size( 'single-puta-portadazza', 354, 354, false );
 }
 
 // Register Nav Menus
