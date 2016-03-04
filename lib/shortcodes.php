b82988c191cacbaa2ec42ac1d64a9b7a3de2db43
Patrick Best <patrickbest@patrickbest.com>
2016-03-04 17:47:33 -0600

    add soundcloud embeds

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -1,17 +1,6 @@
 <?php
 
-// [bartag foo="foo-value"]
-/*
-function bartag_func( $atts ) {
-    $a = shortcode_atts( array(
-        'foo' => 'something',
-        'bar' => 'something else',
-    ), $atts );
-
-    return "foo = {$a['foo']}";
-}
-add_shortcode( 'bartag', 'bartag_func' );
-*/
+// YOUTUBE
 
 function embed_youtube_shortcode($atts) {
   $a = shortcode_atts( array(
@@ -42,3 +31,35 @@ function embed_youtube_shortcode_ad($atts) {
   return $html;
 }
 add_shortcode( 'embed_youtube_with_ad', 'embed_youtube_shortcode_ad' );
+
+// SOUNDLCLOUD
+
+function embed_soundcloud_shortcode($atts) {
+  $a = shortcode_atts( array(
+    'url' => false,
+  ), $atts );
+
+  if (!$a['url']) {
+    return '';
+  }
+
+  $html = '<div class="custom-embed"><div class="wide-soundcloud"><iframe src="https://w.soundcloud.com/player/?url=' . $a['url'] . '&amp;auto_play=false&amp;hide_related=true&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false&amp;visual=true" width="100%" height="200" frameborder="no" scrolling="no"></iframe></div></div>';
+
+  return $html;
+}
+add_shortcode( 'embed_soundcloud', 'embed_soundcloud_shortcode' );
+
+function embed_soundcloud_shortcode_ad($atts) {
+  $a = shortcode_atts( array(
+    'url' => false,
+  ), $atts );
+
+  if (!$a['url']) {
+    return '';
+  }
+
+  $html = '<div class="custom-embed-with-ad u-cf"><div class="embed"><div class="square-soundcloud"><iframe src="https://w.soundcloud.com/player/?url=' . $a['url'] . '&amp;auto_play=false&amp;hide_related=true&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false&amp;visual=true" width="100%" height="450" frameborder="no" scrolling="no"></iframe></div></div><img class="embed-ad" src="https://placeholdit.imgix.net/~text?txtsize=50&txt=AD&w=400&h=400"></div>';
+
+  return $html;
+}
+add_shortcode( 'embed_soundcloud_with_ad', 'embed_soundcloud_shortcode_ad' );
