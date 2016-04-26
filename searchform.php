a108d78c2694680f3c9881027a749319bc971920
cas <casska@gmail.com>
2016-04-26 16:24:14 -0500

    fix searchform placeholder

--- searchform.php
+++ searchform.php
@@ -3,7 +3,7 @@
     <div class="col s2">
     </div>
     <div id="search-field-holder" class="col s16">
-      <input type="search" id="search-field" placeholder="keyword" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+      <input type="search" id="search-field" placeholder="Buscar" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
     </div>
     <div id="search-submit-holder" class="col s4">
       <button type="submit" id="search-submit"><span class="genericon genericon-search"></span></button>
